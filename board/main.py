542e24a8960fa24ef248c9fd9e7592c4e93d1286
paulober <44974737+paulober@users.noreply.github.com>
2022-08-08 20:14:55 +0200

    Stubber version 1.5.7 minified with main.py launcher

--- board/main.py
+++ board/main.py
@@ -1,18 +1,18 @@
+import uos as os
 import time
-from createstubs import Stubber
+
 
 def countdown():
     for i in range(5, 0, -1):
-        print('start stubbing in {}...'.format(i))
+        print("start stubbing in {}...".format(i))
         time.sleep(1)
+    import createstubs
 
-    stubber=Stubber()
 
-    # Add Pico module
-    stubber.add_modules(['rp2'])
-    
-    stubber.clean()
-    stubber.create_all_stubs()
-    stubber.report()
+try:
+    # only run import if no stubs yet
+    os.listdir("stubs")
+    print("stub folder was found, stubbing is not automatically started")
 
-countdown()
+except OSError:
+    countdown()
