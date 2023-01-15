f33d5ccdd9852fd8375bbabb64d5b778e112100a
paulober <44974737+paulober@users.noreply.github.com>
2023-01-15 21:47:12 +0100

    Firmware 1.19.1-796 unstable

--- generated/micropython.py
+++ generated/micropython.py
@@ -1,7 +1,7 @@
 """
-Module: 'micropython' on micropython-v1.19.1-780-rp2
+Module: 'micropython' on micropython-v1.19.1-796-rp2
 """
-# MCU: {'ver': 'v1.19.1-780', 'build': '780', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.19.1', 'release': '1.19.1', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
+# MCU: {'ver': 'v1.19.1-796', 'build': '796', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.19.1', 'release': '1.19.1', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
 # Stubber: 1.5.7
 from typing import Any
 
