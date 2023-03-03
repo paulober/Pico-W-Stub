ac866278b6a9bf49f196fea77e276fcfdc1f1e51
paulober <44974737+paulober@users.noreply.github.com>
2023-03-03 16:12:44 +0100

    Firmware 1.19.1-915 nightly

--- generated/network.py
+++ generated/network.py
@@ -1,7 +1,7 @@
 """
-Module: 'network' on micropython-v1.19.1-796-rp2
+Module: 'network' on micropython-v1.19.1-915-rp2
 """
-# MCU: {'ver': 'v1.19.1-796', 'build': '796', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.19.1', 'release': '1.19.1', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
+# MCU: {'ver': 'v1.19.1-915', 'build': '915', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.19.1', 'release': '1.19.1', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
 # Stubber: 1.5.7
 from typing import Any
 
@@ -50,6 +50,12 @@ class WLAN():
 
     def status(self, *args, **kwargs) -> Any:
         ...
+
+def country(*args, **kwargs) -> Any:
+    ...
+
+def hostname(*args, **kwargs) -> Any:
+    ...
 
 def route(*args, **kwargs) -> Any:
     ...
--- generated/umachine.py
+++ generated/umachine.py
@@ -1,7 +1,7 @@
 """
-Module: 'umachine' on micropython-v1.19.1-796-rp2
+Module: 'umachine' on micropython-v1.19.1-915-rp2
 """
-# MCU: {'ver': 'v1.19.1-796', 'build': '796', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.19.1', 'release': '1.19.1', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
+# MCU: {'ver': 'v1.19.1-915', 'build': '915', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.19.1', 'release': '1.19.1', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
 # Stubber: 1.5.7
 from typing import Any
 
@@ -112,6 +112,15 @@ class Pin():
         ...
 
     ALT = 3 # type: int
+    ALT_GPCK = 8 # type: int
+    ALT_I2C = 3 # type: int
+    ALT_PIO0 = 6 # type: int
+    ALT_PIO1 = 7 # type: int
+    ALT_PWM = 4 # type: int
+    ALT_SIO = 5 # type: int
+    ALT_SPI = 1 # type: int
+    ALT_UART = 2 # type: int
+    ALT_USB = 9 # type: int
     IN = 0 # type: int
     IRQ_FALLING = 4 # type: int
     IRQ_RISING = 8 # type: int
@@ -119,6 +128,79 @@ class Pin():
     OUT = 1 # type: int
     PULL_DOWN = 2 # type: int
     PULL_UP = 1 # type: int
+
+    class board():
+        def __init__(self, *argv, **kwargs) -> None:
+            ...
+
+        GP0 : Any ## <class 'Pin'> = Pin(GPIO0, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP1 : Any ## <class 'Pin'> = Pin(GPIO1, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP10 : Any ## <class 'Pin'> = Pin(GPIO10, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP11 : Any ## <class 'Pin'> = Pin(GPIO11, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP12 : Any ## <class 'Pin'> = Pin(GPIO12, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP13 : Any ## <class 'Pin'> = Pin(GPIO13, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP14 : Any ## <class 'Pin'> = Pin(GPIO14, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP15 : Any ## <class 'Pin'> = Pin(GPIO15, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP16 : Any ## <class 'Pin'> = Pin(GPIO16, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP17 : Any ## <class 'Pin'> = Pin(GPIO17, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP18 : Any ## <class 'Pin'> = Pin(GPIO18, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP19 : Any ## <class 'Pin'> = Pin(GPIO19, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP2 : Any ## <class 'Pin'> = Pin(GPIO2, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP20 : Any ## <class 'Pin'> = Pin(GPIO20, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP21 : Any ## <class 'Pin'> = Pin(GPIO21, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP22 : Any ## <class 'Pin'> = Pin(GPIO22, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP26 : Any ## <class 'Pin'> = Pin(GPIO26, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP27 : Any ## <class 'Pin'> = Pin(GPIO27, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP28 : Any ## <class 'Pin'> = Pin(GPIO28, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP3 : Any ## <class 'Pin'> = Pin(GPIO3, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP4 : Any ## <class 'Pin'> = Pin(GPIO4, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP5 : Any ## <class 'Pin'> = Pin(GPIO5, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP6 : Any ## <class 'Pin'> = Pin(GPIO6, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP7 : Any ## <class 'Pin'> = Pin(GPIO7, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP8 : Any ## <class 'Pin'> = Pin(GPIO8, mode=ALT, pull=PULL_DOWN, alt=31)
+        GP9 : Any ## <class 'Pin'> = Pin(GPIO9, mode=ALT, pull=PULL_DOWN, alt=31)
+        LED : Any ## <class 'Pin'> = Pin(EXT_GPIO0, mode=IN)
+        WL_GPIO0 : Any ## <class 'Pin'> = Pin(EXT_GPIO0, mode=IN)
+        WL_GPIO1 : Any ## <class 'Pin'> = Pin(EXT_GPIO1, mode=IN)
+        WL_GPIO2 : Any ## <class 'Pin'> = Pin(EXT_GPIO2, mode=IN)
+
+    class cpu():
+        def __init__(self, *argv, **kwargs) -> None:
+            ...
+
+        EXT_GPIO0 : Any ## <class 'Pin'> = Pin(EXT_GPIO0, mode=IN)
+        EXT_GPIO1 : Any ## <class 'Pin'> = Pin(EXT_GPIO1, mode=IN)
+        EXT_GPIO2 : Any ## <class 'Pin'> = Pin(EXT_GPIO2, mode=IN)
+        GPIO0 : Any ## <class 'Pin'> = Pin(GPIO0, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO1 : Any ## <class 'Pin'> = Pin(GPIO1, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO10 : Any ## <class 'Pin'> = Pin(GPIO10, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO11 : Any ## <class 'Pin'> = Pin(GPIO11, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO12 : Any ## <class 'Pin'> = Pin(GPIO12, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO13 : Any ## <class 'Pin'> = Pin(GPIO13, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO14 : Any ## <class 'Pin'> = Pin(GPIO14, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO15 : Any ## <class 'Pin'> = Pin(GPIO15, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO16 : Any ## <class 'Pin'> = Pin(GPIO16, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO17 : Any ## <class 'Pin'> = Pin(GPIO17, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO18 : Any ## <class 'Pin'> = Pin(GPIO18, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO19 : Any ## <class 'Pin'> = Pin(GPIO19, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO2 : Any ## <class 'Pin'> = Pin(GPIO2, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO20 : Any ## <class 'Pin'> = Pin(GPIO20, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO21 : Any ## <class 'Pin'> = Pin(GPIO21, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO22 : Any ## <class 'Pin'> = Pin(GPIO22, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO23 : Any ## <class 'Pin'> = Pin(GPIO23, mode=ALT, alt=31)
+        GPIO24 : Any ## <class 'Pin'> = Pin(GPIO24, mode=ALT, alt=31)
+        GPIO25 : Any ## <class 'Pin'> = Pin(GPIO25, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO26 : Any ## <class 'Pin'> = Pin(GPIO26, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO27 : Any ## <class 'Pin'> = Pin(GPIO27, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO28 : Any ## <class 'Pin'> = Pin(GPIO28, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO29 : Any ## <class 'Pin'> = Pin(GPIO29, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO3 : Any ## <class 'Pin'> = Pin(GPIO3, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO4 : Any ## <class 'Pin'> = Pin(GPIO4, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO5 : Any ## <class 'Pin'> = Pin(GPIO5, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO6 : Any ## <class 'Pin'> = Pin(GPIO6, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO7 : Any ## <class 'Pin'> = Pin(GPIO7, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO8 : Any ## <class 'Pin'> = Pin(GPIO8, mode=ALT, pull=PULL_DOWN, alt=31)
+        GPIO9 : Any ## <class 'Pin'> = Pin(GPIO9, mode=ALT, pull=PULL_DOWN, alt=31)
     def high(self, *args, **kwargs) -> Any:
         ...
 
