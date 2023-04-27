0d1f6fb9369a259316e16cf32b8a1d298ef0621e
paulober <44974737+paulober@users.noreply.github.com>
2023-04-27 11:06:23 +0200

    Firmware 1.20.0 stable

--- generated/uheapq.py
+++ generated/uheapq.py
@@ -1,7 +1,7 @@
 """
-Module: 'uheapq' on micropython-v1.19.1-rp2
+Module: 'uheapq' on micropython-v1.20-rp2
 """
-# MCU: {'ver': 'v1.19.1', 'build': '1009', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.19.1', 'release': '1.19.1', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
+# MCU: {'ver': 'v1.20', 'build': '', 'sysname': 'rp2', 'platform': 'rp2', 'version': '1.20.0', 'release': '1.20.0', 'port': 'rp2', 'family': 'micropython', 'name': 'micropython', 'machine': 'Raspberry Pi Pico W with RP2040', 'nodename': 'rp2'}
 # Stubber: 1.5.7
 from typing import Any
 
